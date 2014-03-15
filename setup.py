ae28e4dada7248161956d9dbca0a2c18b541fab8
Gianluca Tiepolo <tiepolo.gian@gmail.com>
2014-03-15 00:27:15 +0000

    Updated

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name = 'thumbS3',
+    packages = ['thumbS3'], 
+    version = '0.1.1',
+    description = 'Python module and command-line tool for creating thumbs on Amazon S3',
+    author = 'Gianluca Tiepolo',
+    author_email = 'tiepolo.gian@gmail.com',
+    url = 'https://github.com/tiepologian/thumbS3',  
+    keywords = ['s3', 'thumbs', 'aws'], 
+    classifiers = [],
+    install_requires=[
+        "argparse >= 1.2.1",
+        "boto >= 2.2.2",
+	"PIL >= 1.1.7",
+	"Pillow >= 2.3.0",
+	"requests >= 2.2.1",
+    ],
+)
